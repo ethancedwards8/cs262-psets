5322969f2ebca0522fcfc3168667d8e1f075520b
Eddie Kohler <ekohler@gmail.com>
2026-03-14 16:20:13 -0400

    Handout code for pset 3

---
+++ pset3/TURNIN.md
@@ -0,0 +1,15 @@
+Problem set 3 turnin
+====================
+
+Describe your turnin. What features did you add? And make sure you fill out the
+“Bug” section!
+
+(You can also keep a lab notebook in NOTEBOOK.md.)
+
+Bug
+---
+
+At some point during your work, you will have found a bug in your
+implementation. D this bug, give a commit hash that demonstrates the bug, and
+describe how you found it and how you fixed it. Give specific pt-paxos arguments
+that demonstrate the error.
---
+++ pset3/pt-paxos.cc
@@ -0,0 +1,243 @@
+#include "lockseq_model.hh"
+#include "pancydb.hh"
+#include "netsim.hh"
+
+namespace cot = cotamer;
+using namespace std::chrono_literals;
+
+// testinfo
+//    Holds configuration information about this test.
+
+struct testinfo {
+    random_source randomness;
+    double loss = 0.0;
+    bool verbose = false;
+    bool print_db = false;
+    size_t nreplicas = 3;
+    size_t initial_leader = 0;
+
+    template <typename T>
+    void configure_port(netsim::port<T>& port) {
+        port.set_verbose(verbose);
+    }
+    template <typename T>
+    void configure_channel(netsim::channel<T>& chan) {
+        chan.set_loss(loss);
+        chan.set_verbose(verbose);
+    }
+    template <typename T>
+    void configure_quiet_channel(netsim::channel<T>& chan) {
+        chan.set_loss(loss);
+    }
+};
+
+
+// pt_paxos_replica, pt_paxos_instance
+//    Manage a test of a Paxos-based Pancy service.
+//    Initialization is more complicated than in the simpler settings;
+//    we have a type, `pt_paxos_replica`, that represents a single replica,
+//    and another, `pt_paxos_instance`, that constructs the replica set.
+
+struct pt_paxos_instance;
+
+// The type for inter-replica messages. You will change this!
+using paxos_message = /* YOUR TYPE HERE */ int;
+
+struct pt_paxos_replica {
+    size_t index_;           // index of this replica in the replica set
+    size_t nreplicas_;       // number of replicas
+    size_t leader_index_;    // this replica’s idea of the current leader
+    netsim::port<pancy::request> from_clients_;   // port for client messages
+    netsim::port<paxos_message> from_replicas_;   // port for inter-replica messages
+    netsim::channel<pancy::response> to_clients_; // channel for client responses
+    // channels for inter-replica messages:
+    std::vector<std::unique_ptr<netsim::channel<paxos_message>>> to_replicas_;
+    pancy::pancydb db_;      // our copy of the database
+    // ...plus anything you want to add
+
+    pt_paxos_replica(size_t index, size_t nreplicas, random_source&);
+    void initialize(pt_paxos_instance&);
+
+    cot::task<> run();
+};
+
+struct pt_paxos_instance {
+    testinfo& tester;
+    client_model& clients;
+    std::vector<std::unique_ptr<pt_paxos_replica>> replicas;
+    // ...plus anything you want to add
+
+    pt_paxos_instance(testinfo&, client_model&);
+};
+
+
+// Configuration and initialization
+
+pt_paxos_replica::pt_paxos_replica(size_t index, size_t nreplicas, random_source& randomness)
+    : index_(index),
+      nreplicas_(nreplicas),
+      from_clients_(randomness, std::format("R{}", index_)),
+      from_replicas_(randomness, std::format("R{}/r", index_)),
+      to_clients_(randomness, from_clients_.id()),
+      to_replicas_(nreplicas) {
+    for (size_t s = 0UL; s != nreplicas_; ++s) {
+        to_replicas_[s].reset(new netsim::channel<paxos_message>(
+            randomness, from_clients_.id()
+        ));
+    }
+}
+
+void pt_paxos_replica::initialize(pt_paxos_instance& inst) {
+    leader_index_ = inst.tester.initial_leader;
+    inst.clients.connect_replica(index_, from_clients_, to_clients_);
+    inst.tester.configure_port(from_clients_);
+    inst.tester.configure_port(from_replicas_);
+    inst.tester.configure_channel(to_clients_);
+    inst.tester.configure_quiet_channel(inst.clients.request_channel(index_));
+    for (size_t s = 0UL; s != nreplicas_; ++s) {
+        to_replicas_[s]->connect(inst.replicas[s]->from_replicas_);
+        inst.tester.configure_channel(*to_replicas_[s]);
+    }
+}
+
+pt_paxos_instance::pt_paxos_instance(testinfo& tester, client_model& clients)
+    : tester(tester), clients(clients), replicas(tester.nreplicas) {
+    for (size_t s = 0UL; s != tester.nreplicas; ++s) {
+        replicas[s].reset(new pt_paxos_replica(s, tester.nreplicas, tester.randomness));
+    }
+    for (size_t s = 0UL; s != tester.nreplicas; ++s) {
+        replicas[s]->initialize(*this);
+    }
+}
+
+
+
+// ********** PANCY SERVICE CODE **********
+
+cot::task<> pt_paxos_replica::run() {
+    // Your code here! The handout code just implements a single primary.
+
+    while (true) {
+        // receive message
+        auto req = co_await from_clients_.receive();
+
+        // if not leader, redirect
+        if (index_ != leader_index_) {
+            co_await to_clients_.send(pancy::redirection_response{
+                pancy::response_header(req, pancy::errc::redirect), leader_index_
+            });
+            continue;
+        }
+
+        // if leader, process message and send response
+        co_await to_clients_.send(db_.process_req(req));
+    }
+}
+
+// ******** end Pancy service code ********
+
+
+
+// Test functions
+
+cot::task<> clear_after(cot::duration d) {
+    co_await cot::after(d);
+    cot::clear();
+}
+
+bool try_one_seed(testinfo& tester, unsigned long seed) {
+    cot::reset();   // clear old events and coroutines
+    tester.randomness.seed(seed);
+
+    // Create client generator and test instance
+    lockseq_model clients(tester.nreplicas, tester.randomness);
+    pt_paxos_instance inst(tester, clients);
+
+    // Start coroutines
+    clients.start();
+    std::vector<cot::task<>> tasks;
+    for (size_t s = 0UL; s != tester.nreplicas; ++s) {
+        tasks.push_back(inst.replicas[s]->run());
+    }
+    cot::task<> timeout_task = clear_after(100s);
+
+    // Wait for `timeout_task`
+    cot::loop();
+
+    // Check database
+    std::print("{} lock, {} write, {} clear, {} unlock\n",
+               clients.lock_complete, clients.write_complete,
+               clients.clear_complete, clients.unlock_complete);
+    pancy::pancydb& db = inst.replicas[tester.initial_leader]->db_;
+    if (auto problem = clients.check(db)) {
+        std::print(std::clog, "*** FAILURE on seed {} at key {}\n", seed, *problem);
+        db.print_near(*problem, std::clog);
+        return false;
+    } else if (tester.print_db) {
+        db.print(std::cout);
+    }
+    return true;
+}
+
+
+// Argument parsing
+
+static struct option options[] = {
+    { "count", required_argument, nullptr, 'n' },
+    { "seed", required_argument, nullptr, 'S' },
+    { "random-seeds", required_argument, nullptr, 'R' },
+    { "loss", required_argument, nullptr, 'l' },
+    { "verbose", no_argument, nullptr, 'V' },
+    { "print-db", no_argument, nullptr, 'p' },
+    { "quiet", no_argument, nullptr, 'q' },
+    { nullptr, 0, nullptr, 0 }
+};
+
+int main(int argc, char* argv[]) {
+    testinfo tester;
+
+    std::optional<unsigned long> first_seed;
+    unsigned long seed_count = 1;
+
+    auto shortopts = short_options_for(options);
+    int ch;
+    while ((ch = getopt_long(argc, argv, shortopts.c_str(), options, nullptr)) != -1) {
+        if (ch == 'S') {
+            first_seed = from_str_chars<unsigned long>(optarg);
+        } else if (ch == 'R') {
+            seed_count = from_str_chars<unsigned long>(optarg);
+        } else if (ch == 'l') {
+            tester.loss = from_str_chars<double>(optarg);
+        } else if (ch == 'n') {
+            tester.nreplicas = from_str_chars<size_t>(optarg);
+        } else if (ch == 'V') {
+            tester.verbose = true;
+        } else if (ch == 'p') {
+            tester.print_db = true;
+        } else {
+            std::print(std::cerr, "Unknown option\n");
+            return 1;
+        }
+    }
+
+    bool ok;
+    if (first_seed) {
+        ok = try_one_seed(tester, *first_seed);
+    } else {
+        std::mt19937_64 seed_generator = randomly_seeded<std::mt19937_64>();
+        for (unsigned long i = 0; i != seed_count; ++i) {
+            if (i > 0 && i % 1000 == 0) {
+                std::print(std::cerr, ".");
+            }
+            unsigned long seed = seed_generator();
+            ok = try_one_seed(tester, seed);
+            if (!ok) {
+                break;
+            }
+        }
+        if (ok && seed_count >= 1000) {
+            std::print(std::cerr, "\n");
+        }
+    }
+    return ok ? EXIT_SUCCESS : EXIT_FAILURE;
+}
