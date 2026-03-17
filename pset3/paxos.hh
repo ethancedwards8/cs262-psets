1f42ab0becc140c13af4fbf1cf6c92c69775a63e
Ethan Carter Edwards <ethan@ethancedwards.com>
2026-03-17 15:33:50 -0400

    gee ethan maybe make sure it copmiles

--- pset3/paxos.hh
+++ pset3/paxos.hh
@@ -1,11 +1,12 @@
 #pragma once
 #include <variant>
 #include <deque>
+#include "pancy_msgs.hh"
 
 
 struct base_message {
     unsigned long long round;
-}
+};
 
 struct prepare_msg : base_message {
     unsigned long long committed_slot = 0; // decide shortcut
@@ -15,13 +16,13 @@ struct prepare_msg : base_message {
     unsigned long long prev_round;
 
     unsigned long long batch_start = 0;
-    std::deque<pancy_message> entries; // empty = raft heartbeat keepalive
-}
+    std::deque<pancy::request> entries; // empty = raft heartbeat keepalive
+};
 
 struct ack_msg : base_message {
     bool success = false;
     unsigned long long highest_accepted = 0;
-}
+};
 
 
 using paxos_message = std::variant<prepare_msg, ack_msg>;
